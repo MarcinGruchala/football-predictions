d31d9dc7daf0e19430c059a73713ab1a5b6632ce
Marcin Gruchala <margruchala@gmail.com>
2024-06-29 11:48:30 +0200

    Add convert date functions that supports dirent date formats + add combined data file

---
+++ football_predictions/data/create_interim_premier_league_data.py
@@ -0,0 +1,5 @@
+'''
+Creates combined Premier League data for all seasons in data/raw/premier_league.
+Encodes the data and saves it as a CSV in data/interim/premier_league.
+'''
+
---
+++ football_predictions/data/tools/convert_date.py
@@ -0,0 +1,16 @@
+''' 
+This function converts a date string to a datetime object
+and then back to a string with the format 'dd/mm/yyyy'. 
+'''
+
+import pandas as pd
+
+def convert_date(date_str):
+    '''
+    Convert a date string to a datetime object and then back to a string
+    with the format 'dd/mm/yyyy
+    '''
+    if len(date_str.split('/')[-1]) == 2:
+        return pd.to_datetime(date_str, format='%d/%m/%y').strftime('%d/%m/%Y')
+    else:
+        return pd.to_datetime(date_str, format='%d/%m/%Y').strftime('%d/%m/%Y')
