e9881211bcccfa8ba5019275055249e52c6d11c1
Marcin Gruchala <margruchala@gmail.com>
2024-07-14 10:51:00 +0200

    Add legend file for data and rolling averages configuration

--- football_predictions/data/configuration.py
+++ football_predictions/data/configuration.py
@@ -1,3 +1,9 @@
 ''' Configuration file for data collection and processing. '''
 
 SEASONS = ['2324', '2223', '2122', '2021', '1920', '1819', '1718', '1617']
+
+ROLLING_AVERAGES_COLUMNS = [
+    'FTHG', 'FTAG', 'HTHG', 'HTAG',
+    'HS', 'AS', 'HST', 'AST', 'HC', 'AC',
+    'HF', 'AF', 'HY', 'AY', 'HR', 'AR', 'FTR_code', 'HTR_code'
+]
