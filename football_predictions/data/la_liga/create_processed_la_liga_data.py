468353e303ef63df9809a7bed2a8f7bc8f54625b
Marcin Gruchala <margruchala@gmail.com>
2024-07-21 13:48:06 +0200

    Add scripts for generating serie a and la liga processed data

---
+++ football_predictions/data/la_liga/create_processed_la_liga_data.py
@@ -0,0 +1,7 @@
+'''
+This script is used to create the processed data for the La Liga.
+'''
+
+from ..tools.processed import create_processed_data_for_league
+
+create_processed_data_for_league('la_liga')
