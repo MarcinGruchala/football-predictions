ed3494624a1b909e0f64564e0fc7bf53bf17df59
Marcin Gruchala <margruchala@gmail.com>
2024-07-28 13:52:15 +0200

    Fix running imports in data_preparation.py and clean up premier league tools.py

--- football_predictions/data/data_preparation.py
+++ football_predictions/data/data_preparation.py
@@ -6,9 +6,9 @@ from .configuration import LEAGUES, RAW_DATA_PATH, INTERIM_DATA_PATH, PROCESSED_
 from .tools.raw import download_raw_data_for_league
 from .tools.interim import create_interim_data_for_league
 from .tools.processed import create_processed_data_for_league
-from .serie_a.download_raw_serie_a_data import download_serie_a_season_data
-from .premier_league.download_raw_premier_league_data import download_premier_league_season_data
-from .la_liga.download_raw_la_liga_data import download_la_liga_season_data
+from .premier_league.tools import download_premier_league_season_data
+from .la_liga.tools import download_la_liga_season_data
+from .serie_a.tools import download_serie_a_season_data
 
 # Function to combine all *_combined.csv files in a folder
 def combine_csv_files(main_folder):
@@ -24,23 +24,28 @@ def combine_csv_files(main_folder):
     combined_df_inner = pd.concat(combined_df_list, ignore_index=True)
     return combined_df_inner
 
-# Download raw data for leagues from configuration file
-# Because each league has slightly different download URL I have to do it manually
-download_raw_data_for_league('serie_a',download_serie_a_season_data)
-download_raw_data_for_league('premier_league',download_premier_league_season_data)
-download_raw_data_for_league('la_liga',download_la_liga_season_data)
+def main():
+    ''' Main function to prepare data for all leagues '''
+    # Download raw data for leagues from configuration file
+    # Because each league has slightly different download URL I have to do it manually
+    download_raw_data_for_league('serie_a',download_serie_a_season_data)
+    download_raw_data_for_league('premier_league',download_premier_league_season_data)
+    download_raw_data_for_league('la_liga',download_la_liga_season_data)
 
-for league in LEAGUES:
-    create_interim_data_for_league(league)
+    for league in LEAGUES:
+        create_interim_data_for_league(league)
 
-for league in LEAGUES:
-    create_processed_data_for_league(league)
+    for league in LEAGUES:
+        create_processed_data_for_league(league)
 
-# Combine all *_combined.csv files from interim, processed, and raw folders
-print('*** Combining all *_combined.csv files ***')
-folders_to_combine = [RAW_DATA_PATH, INTERIM_DATA_PATH, PROCESSED_DATA_PATH]
-for folder in folders_to_combine:
-    combined_df = combine_csv_files(folder)
-    combined_df.to_csv(f'{folder}_combined.csv', index=False)
-print('*** Combining all *_combined.csv files complete ***')
-print('*** Data preparation complete ***')
+    # Combine all *_combined.csv files from interim, processed, and raw folders
+    print('*** Combining all *_combined.csv files ***')
+    folders_to_combine = [RAW_DATA_PATH, INTERIM_DATA_PATH, PROCESSED_DATA_PATH]
+    for folder in folders_to_combine:
+        combined_df = combine_csv_files(folder)
+        combined_df.to_csv(f'{folder}_combined.csv', index=False)
+    print('*** Combining all *_combined.csv files complete ***')
+    print('*** Data preparation complete ***')
+
+if __name__ == "__main__":
+    main()
--- football_predictions/data/premier_league/tools.py
+++ football_predictions/data/premier_league/tools.py
@@ -2,35 +2,6 @@
 
 import pandas as pd
 
-def download_premier_league_base_data():
-    """
-    Downloads and combines Premier League season data from multiple seasons.
-
-    Returns:
-      combined (pandas.DataFrame): Combined data frame containing Premier League season data.
-    """
-    premier_league_2324 = download_premier_league_season_data('2324')
-    premier_league_2223 = download_premier_league_season_data('2223')
-    premier_league_2122 = download_premier_league_season_data('2122')
-    premier_league_2021 = download_premier_league_season_data('2021')
-    premier_league_1920 = download_premier_league_season_data('1920')
-    premier_league_1819 = download_premier_league_season_data('1819')
-    premier_league_1718 = download_premier_league_season_data('1718')
-    # Combine the data frames into one
-    combined = pd.concat([
-      premier_league_1920,
-      premier_league_2021,
-      premier_league_2122,
-      premier_league_2223,
-      premier_league_2324,
-      premier_league_1819,
-      premier_league_1718
-      ])
-    # Reset the index to avoid any potential issues
-    combined.reset_index(drop=True, inplace=True)
-
-    return combined
-
 def download_premier_league_season_data(season: str):
     """
     Downloads the Premier League season data for the specified season.
